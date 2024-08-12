861a37719bb52cd36edb2062cd6ca5676e78efcf
garlic-brewlabs <gavin.jaynes@gmail.com>
2024-08-12 23:21:41 +1000

    Theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,21 @@ import ReactDOM from "react-dom/client";
 
 import "@/styles.css";
 import App from "@/App.tsx";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/sonner";
 import { ProviderAppKit } from "@/components/provider-appkit.tsx";
-import { MoralisProvider } from "@/components/moralis-provider.tsx";
+import { ProviderMoralis } from "@/components/provider-moralis";
+
+import { ThemeProvider } from "@/components/provider-theme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ProviderAppKit>
-      <MoralisProvider>
-        <App />
-        <Toaster />
-      </MoralisProvider>
-    </ProviderAppKit>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ProviderAppKit>
+        <ProviderMoralis>
+          <App />
+          <Toaster />
+        </ProviderMoralis>
+      </ProviderAppKit>
+    </ThemeProvider>
   </React.StrictMode>
 );

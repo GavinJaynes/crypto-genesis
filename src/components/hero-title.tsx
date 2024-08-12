71c7827c8d929093349f17695f29d329017f829e
garlic-brewlabs <gavin.jaynes@gmail.com>
2024-08-12 23:22:19 +1000

    Update hero-title.tsx

--- src/components/hero-title.tsx
+++ src/components/hero-title.tsx
@@ -1,10 +1,12 @@
 import { FlipWords } from "@/components/ui/flip-words";
+import GlowCircle from "./ui/glow-circle";
 
 const HeroTitle = () => {
   const words = ["better", "sleek", "beautiful", "modern"];
 
   return (
     <div className="flex justify-center items-center">
+      <GlowCircle />
       <div className="sm:text-6xl text-5xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
         Build
         <FlipWords words={words} /> <br />

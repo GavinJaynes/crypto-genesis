e2372ba3f81ed4ce2fff15b5fd2355faca46dd05
garlic-brewlabs <garlic@brewlabs.info>
2024-08-14 13:13:00 +1000

    More precise numbers

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -13,7 +13,7 @@ export const formatNumber = (number: number) => {
   return number.toLocaleString("en-US", {
     // add suffixes for thousands, millions, and billions
     // the maximum number of decimal places to use
-    maximumFractionDigits: 2,
+    maximumFractionDigits: 3,
     // specify the abbreviations to use for the suffixes
     notation: "compact",
     compactDisplay: "short",

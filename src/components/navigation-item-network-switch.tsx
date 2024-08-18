d74ee7895c9679e1ee1ab5e22fa818b324252156
garlic-brewlabs <88916999+garlic-brewlabs@users.noreply.github.com>
2024-08-18 22:45:06 +1000

    Using Apppkits change network in nav

--- src/components/navigation-item-network-switch.tsx
+++ src/components/navigation-item-network-switch.tsx
@@ -5,32 +5,40 @@ import Bsc from "@/components/icons/bsc";
 import Ethereum from "@/components/icons/ethereum";
 import Arbitrum from "@/components/icons/arbitrum";
 
+import { ButtonModal } from "./button-modal";
+
 const NavigationItemNetworkSwitch = () => {
   const { chains, switchChain } = useSwitchChain();
 
   return (
-    <div>
-      <h5 className="text-sm font-medium leading-none gap-2 flex items-center">
-        <ArrowLeftRightIcon size={14} /> Switch networks
-      </h5>
+    <div className="flex flex-col gap-2">
+      <ButtonModal
+        screen="Networks"
+        variant="link"
+        className="flex flex-col text-left w-fit p-0 m-0 h-fit"
+      >
+        <h5 className="text-sm font-medium leading-none gap-2 flex items-center">
+          <ArrowLeftRightIcon size={14} /> Switch networks
+        </h5>
+      </ButtonModal>
 
-      <div className="line-clamp-2 text-sm leading-snug text-muted-foreground mt-2">
-        <p>Change to another supported network</p>
+      <p className="line-clamp-2 text-sm leading-snug text-muted-foreground ">
+        Change to another supported network
+      </p>
 
-        <div className="flex gap-4 mt-1">
-          {chains.map((chain) => (
-            <button
-              key={chain.id}
-              className="grayscale hover:grayscale-0 transition-all duration-300"
-              onClick={() => switchChain({ chainId: chain.id })}
-            >
-              {chain.id === 56 && <Bsc />}
-              {chain.id === 1 && <Ethereum />}
-              {chain.id === 42161 && <Arbitrum />}
-              <span className="sr-only">Change network to {chain.name}</span>
-            </button>
-          ))}
-        </div>
+      <div className="flex gap-4 mt-1">
+        {chains.map((chain) => (
+          <button
+            key={chain.id}
+            className="grayscale hover:grayscale-0 transition-all duration-300"
+            onClick={() => switchChain({ chainId: chain.id })}
+          >
+            {chain.id === 56 && <Bsc />}
+            {chain.id === 1 && <Ethereum />}
+            {chain.id === 42161 && <Arbitrum />}
+            <span className="sr-only">Change network to {chain.name}</span>
+          </button>
+        ))}
       </div>
     </div>
   );
